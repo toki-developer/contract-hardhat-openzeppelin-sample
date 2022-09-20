9c9cbc3a43e60b5bb91bb7ab2a387cd9ab96aa69
Toki <55911995+tokio-k@users.noreply.github.com>
2022-09-20 09:13:03 +0900

    test upgrade contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -3,18 +3,35 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+import "hardhat/console.sol";
+
 contract Box is OwnableUpgradeable {
     uint256 private _value;
+    uint256 public testval;
 
     event ValueChanged(uint256 value);
 
-    function store(uint256 value) public {
+    function initialize(uint256 value) public initializer {
+        _value = value;
+        testval = 52;
+        __Ownable_init_unchained();
+    }
 
+    function store(uint256 value) public onlyOwner {
         _value = value;
         emit ValueChanged(value);
     }
 
-    function retrive() public onlyOwner view returns(uint256){
+    function retrive() public view returns(uint256){
+        console.log("--==--");
+        console.log("success upgrade -==-===");
+        console.log("--==--");
         return _value;
+    }
+
+    function upgradeSuccess() public pure returns(string memory) {
+        return "successsss";
     }
 }
--- scripts/upgrade_upgradeable_box.js
+++ scripts/upgrade_upgradeable_box.js
@@ -2,8 +2,8 @@ const { ethers, upgrades } = require('hardhat');
 
 async function main () {
   const Box = await ethers.getContractFactory('Box');
-  console.log('Deploying Box...');
-  const box = await upgrades.deployProxy(Box, [50], { initializer: 'initialize' });
+  console.log('Upgrade Box...');
+  const box = await upgrades.upgradeProxy("0x0B306BF915C4d645ff596e518fAf3F9669b97016",Box);
   await box.deployed();
   console.log('Box deployed to:', box.address);
 }
